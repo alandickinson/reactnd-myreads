e580f952ed64e2a254d48a1278fd912ee33fa122
alandickinson <patrick.alan@gmail.com>
2017-08-23 23:41:12 -0400

    refactor: moves book code into its own component

--- src/BookList.js
+++ src/BookList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Book from './Book'
 
 class BookList extends Component {
   static propTypes = {
@@ -15,29 +16,7 @@ class BookList extends Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {this.props.books.map((book) => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    {book.imageLinks && (
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                    )}
-                    {!book.imageLinks && (
-                      <div className="book-cover" style={{ width: 128, height: 193}}></div>
-                    )}
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors.join(', ')}</div>
-                </div>
-              </li>
+              <Book key={book.id} book={book}/>
             ))}
           </ol>
         </div>
